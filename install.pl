d17cf0092c8bd44d0c2065fc76024c052f3d6cb4
Alexander S. Klenin <klenin@gmail.com>
2015-07-12 20:42:04 +1000

    Add installation script

---
+++ install.pl
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+
+use File::Copy qw(copy);
+use File::Spec;
+
+print "Installing cats-judge\n";
+
+my $step_count = 0;
+
+sub step($&) {
+    my ($msg, $action) = @_;
+    print ++$step_count, ". $msg ...";
+    flush STDOUT;
+    $action->();
+    print " ok\n";
+}
+
+sub step_copy {
+    my ($from, $to) = @_;
+    step "Copying $from -> $to", sub {
+        -e $to and die "Destination already exists: $to";
+        copy($from, $to) or die $!;
+    };
+}
+
+step 'Verifying', sub {
+    -f 'judge.pl' && -d 'lib' or die 'Must run from cats-judge directory';
+    -f 'config.xml' and die 'Seems to be already installed';
+};
+
+step 'Verifying git', sub {
+    my $x = `git --version` or die 'Git not found';
+    $x =~ /^git version/ or die "Git not found: $x";
+};
+
+step 'Cloning sumbodules', sub {
+    `git submodule update --init` or die $!;
+};
+
+my @p = qw(lib cats-problem CATS);
+step_copy(File::Spec->catfile(@p, 'Config.pm.template'), File::Spec->catfile(@p, 'Config.pm'));
+
+step_copy('config.xml.template', 'config.xml');
