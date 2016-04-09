klenin/cats-judge

commit 041a0744f29b2270c1aa342a072d65e9c7ae6289

Split windows-specific DE detector code

install.pl
@@ -95,14 +95,14 @@ step 'Cloning sumbodules', sub {
 };
 
 step 'Disabling Windows Error Reporting UI', sub {
-    CATS::DevEnv::Detector::Utils::disable_windows_error_reporting_ui;
+    CATS::DevEnv::Detector::Utils::disable_windows_error_reporting_ui();
 };
 
 my @detected_DEs;
 step 'Detecting development environments', sub {
     IPC::Cmd->can_capture_buffer or die 'IPC::Cmd failed';
     print "\n";
-    CATS::DevEnv::Detector::Utils::disable_error_dialogs;
+    CATS::DevEnv::Detector::Utils::disable_error_dialogs();
     for (globq(File::Spec->catfile(qw[lib CATS DevEnv Detector *.pm]))) {
         my ($name) = /(\w+)\.pm$/;
         next if $name =~ /^(Utils|Base)$/ || $opts{devenv} && $name !~ qr/$opts{devenv}/i;

lib/CATS/DevEnv/Detector/Utils.pm
@@ -3,276 +3,15 @@ package CATS::DevEnv::Detector::Utils;
 use strict;
 use warnings;
 
-use if $^O eq 'MSWin32', 'Win32::TieRegistry';
-use if $^O eq 'MSWin32', 'Win32::API';
-use if $^O eq 'MSWin32', 'Win32API::File' => qw(getLogicalDrives SetErrorMode);
-
-use Encode;
-use File::Glob 'bsd_glob';
-use File::Spec;
-use File::Path qw(remove_tree);
-use constant FS => 'File::Spec';
-
-use constant TEMP_SUBDIR => 'tmp';
+use CATS::DevEnv::Detector::Utils::Common;
+use if $^O eq 'MSWin32', 'CATS::DevEnv::Detector::Utils::Windows';
+use if $^O ne 'MSWin32', 'CATS::DevEnv::Detector::Utils::WindowsStub';
 
 use parent qw(Exporter);
 our @EXPORT = qw(
     TEMP_SUBDIR temp_file write_temp_file version_cmp clear normalize_path globq
-    which env_path folder registry registry_assoc registry_glob program_files drives lang_dirs
-    debug_log
+    which env_path folder debug_log
+    registry registry_assoc registry_glob program_files drives lang_dirs
 );
-
-our ($log, $debug);
-
-sub debug_log { print $log @_, "\n" if $debug; }
-
-sub globq {
-    my ($pattern) = @_;
-    $pattern =~ s/\\/\\\\/g;
-    bsd_glob $pattern;
-}
-
-sub clear { remove_tree(TEMP_SUBDIR, { error => \my $err }) }
-
-sub temp_file { FS->rel2abs(FS->catfile(TEMP_SUBDIR, $_[0])) }
-
-sub write_temp_file {
-    my ($name, $text) = @_;
-    -d TEMP_SUBDIR or mkdir TEMP_SUBDIR;
-    my $file = temp_file($name);
-    open my $fh, '>', $file;
-    print $fh $text;
-    close $fh;
-    return $file;
-}
-
-sub which {
-    my ($detector, $file) = @_;
-    return 0 if $^O eq 'MSWin32';
-    my $output =`which $file`;
-    for my $line (split /\n/, $output) {
-        $detector->validate_and_add($line);
-    }
-}
-
-sub env_path {
-    my ($detector, $file) = @_;
-    for my $dir (FS->path) {
-        folder($detector, $dir, $file);
-    }
-}
-
-sub extension {
-    my ($detector, $path) = @_;
-    my @exts = ('', '.exe', '.bat', '.com');
-    for my $e (@exts) {
-        $detector->validate_and_add($path . $e);
-    }
-}
-
-sub folder {
-    my ($detector, $folder, $file) = @_;
-    debug_log("folder: $folder / $file");
-    for (globq $folder) {
-        extension($detector, FS->catfile($_, $file));
-    }
-}
-
-use constant REGISTRY_PREFIX => qw(
-    HKEY_LOCAL_MACHINE/SOFTWARE/
-    HKEY_LOCAL_MACHINE/SOFTWARE/Wow6432Node/
-);
-
-sub registry {
-    my ($detector, $reg, $key, $local_path, $file) = @_;
-    $local_path ||= '';
-    for my $reg_prefix (REGISTRY_PREFIX) {
-        my $registry = get_registry_obj("$reg_prefix$reg") or next;
-        my $folder = $registry->GetValue($key) or next;
-        folder($detector, FS->catdir($folder, $local_path), $file);
-    }
-}
-
-use constant REG_READONLY => {
-    Delimiter => '/',
-    Access =>
-        Win32::TieRegistry::KEY_READ
-};
-
-use constant REG_READ_WRITE => {
-    Delimiter => '/',
-    Access =>
-        Win32::TieRegistry::KEY_READ |
-        Win32::TieRegistry::KEY_WRITE
-};
-
-sub get_registry_obj {
-    my ($reg) = @_;
-    debug_log("get_registry_obj: $reg");
-    Win32::TieRegistry->new($reg, REG_READONLY);
-}
-
-sub registry_assoc {
-    my ($detector, %p) = @_;
-    my $assoc = $p{assoc} or die;
-    my $local_path = $p{local_path} // '';
-    my $file = $p{file} or die;
-    my $command = $p{command} // 'Open';
-    my $cmd_key = get_registry_obj("HKEY_CLASSES_ROOT/$assoc/Shell/$command/Command") or return;
-    my $cmd_line = $cmd_key->GetValue('') or return;
-    my ($folder) = ($cmd_line =~ /^\"([^"]+)\\[^\\]+\"/) or return;
-    folder($detector, FS->catdir($folder, $local_path), $file);
-}
-
-sub SubKeyNames_fixup {
-    my ($reg) = @_;
-    $reg = tied(%$reg) if tied(%$reg);
-    # Workaround for https://rt.cpan.org/Public/Bug/Display.html?id=97127, fixed in Win32::TieRegistry 0.27
-    use version ();
-    return $reg->SubKeyNames if version->parse($Win32::TieRegistry::VERSION) >= version->parse('0.27');
-    my @subkeys;
-    my ($nameSize, $classSize) = $reg->Information(qw(MaxSubKeyLen MaxSubClassLen));
-    while ($reg->RegEnumKeyEx(
-        scalar @subkeys, my $subkey, (my $ns = $nameSize + 1), [], my $class, (my $cl = $classSize + 1), my $time)
-    ) {
-        push @subkeys, $subkey;
-    }
-    @subkeys;
-}
-
-sub _registry_rec {
-    my ($detector, $parent, $local_path, $file, $key, @rest) = @_;
-    if (!@rest) {
-        my $folder = $parent->GetValue($key) or return;
-        folder($detector, FS->catdir($folder, $local_path), $file);
-        return;
-    }
-    my @names = $key eq '*' ? SubKeyNames_fixup($parent) : $key;
-    for my $subkey (@names) {
-        my $subreg = $parent->Open($subkey, REG_READONLY) or next;
-        debug_log('_registry_rec: ', $parent->Path(), $subkey);
-        _registry_rec($detector, $subreg, $local_path, $file, @rest)
-    }
-}
-
-sub registry_glob {
-    my ($detector, $reg_path, $local_path, $file) = @_;
-    $local_path ||= '';
-    my @r = split '/', $reg_path, -1;
-    for (REGISTRY_PREFIX) {
-        my $reg = get_registry_obj($_) or return;
-        _registry_rec($detector, $reg, $local_path, $file, @r);
-    }
-}
-
-sub program_files {
-    my ($detector, $local_path, $file) = @_;
-    my @keys = (
-        'ProgramFilesDir',
-        'ProgramFilesDir (x86)'
-    );
-    foreach my $key (@keys) {
-        registry($detector, 'Microsoft/Windows/CurrentVersion', $key, $local_path, $file);
-    }
-}
-
-sub drives {
-    my ($detector, $folder, $file) = @_;
-    $^O eq 'MSWin32' or return;
-    $folder or die;
-    my @drives = getLogicalDrives();
-    foreach my $drive (@drives) {
-        folder($detector, FS->catdir($drive, $folder), $file);
-    }
-}
-
-sub lang_dirs {
-    my ($detector, $folder, $subfolder, $file) = @_;
-    drives($detector, FS->catfile(@$_), $file) for
-        [ 'lang', $folder, $subfolder ],
-        [ 'langs', $folder, $subfolder ],
-        [ 'lang', $folder, '*', $subfolder ],
-        [ 'langs', $folder, '*', $subfolder ];
-}
-
-sub normalize_path { FS->case_tolerant ? uc $_[0] : $_[0] }
-
-sub version_cmp {
-    my ($a, $b) = @_;
-    my @A = ($a =~ /([-.]|\d+|[^-.\d]+)/g);
-    my @B = ($b =~ /([-.]|\d+|[^-.\d]+)/g);
-
-    my ($A, $B);
-    while (@A and @B) {
-        $A = shift @A;
-        $B = shift @B;
-        if ($A eq '.' and $B eq '.') {
-            next;
-        } elsif ( $A eq '.' ) {
-            return -1;
-        } elsif ( $B eq '.' ) {
-            return 1;
-        } elsif ($A =~ /^\d+$/ and $B =~ /^\d+$/) {
-            if ($A =~ /^0/ || $B =~ /^0/) {
-                return $A cmp $B if $A cmp $B;
-            } else {
-                return $A <=> $B if $A <=> $B;
-            }
-        } else {
-            $A = uc $A;
-            $B = uc $B;
-            return $A cmp $B if $A cmp $B;
-        }
-    }
-    @A <=> @B;
-}
-
-sub disable_error_dialogs {
-    $^O eq 'MSWin32' or return;
-    # https://msdn.microsoft.com/en-us/library/windows/desktop/ms680621%28v=vs.85%29.aspx
-    # SEM_FAILCRITICALERRORS
-    SetErrorMode(1 | SetErrorMode(0));
-}
-
-use constant POLICY_KEYS => qw(
-    HKEY_LOCAL_MACHINE/Software/Policies
-    HKEY_LOCAL_MACHINE/Software/Microsoft/Windows/CurrentVersion/Policies
-    HKEY_CURRENT_USER/Software/Policies
-    HKEY_CURRENT_USER/Software/Microsoft/Windows/CurrentVersion/Policies
-);
-
-sub windows_encode_console_message {
-    my ($msg) = @_;
-    my $acp = Win32::API::More->new('kernel32', 'int GetACP()')->Call;
-    my $cocp = Win32::API::More->new('kernel32', 'int GetConsoleOutputCP()')->Call;
-    Encode::from_to($msg, "CP$acp", "CP$cocp") if $acp != $cocp;
-    $msg;
-}
-
-sub disable_windows_error_reporting_ui {
-    $^O eq 'MSWin32' or return;
-    my $wre = 'Microsoft/Windows/Windows Error Reporting';
-    for my $key (POLICY_KEYS) {
-        my $obj = get_registry_obj("$key/$wre") or next;
-        my $old_value = $obj->GetValue('DontShowUI') // next;
-        if (hex($old_value) > 0) {
-            print ' (already disabled) ';
-        }
-        else {
-            print ' (was enabled) ';
-            $obj = Win32::TieRegistry->new("$key/$wre", REG_READ_WRITE) or die $^E;
-            $obj->SetValue('DontShowUI', 1);
-        }
-        return;
-    }
-    print ' (was undefined) ';
-    for my $key (POLICY_KEYS) {
-        my $parent = Win32::TieRegistry->new($key, REG_READ_WRITE) or next;
-        my $obj = $parent->CreateKey($wre, REG_READ_WRITE) or next;
-        $obj->SetValue('DontShowUI', 1) or next;
-        return;
-    }
-    print 'failed to set: ', windows_encode_console_message($^E);
-}
 
 1;

lib/CATS/DevEnv/Detector/Utils/Common.pm
@@ -1,0 +1,108 @@
+package CATS::DevEnv::Detector::Utils::Common;
+
+use strict;
+use warnings;
+
+use Encode;
+use File::Glob 'bsd_glob';
+use File::Spec;
+use File::Path qw(remove_tree);
+use constant FS => 'File::Spec';
+
+use constant TEMP_SUBDIR => 'tmp';
+
+use parent qw(Exporter);
+our @EXPORT = qw(
+    TEMP_SUBDIR temp_file write_temp_file version_cmp clear normalize_path globq
+    which env_path folder debug_log
+);
+
+our ($log, $debug);
+
+sub debug_log { print $log @_, "\n" if $debug; }
+
+sub globq {
+    my ($pattern) = @_;
+    $pattern =~ s/\\/\\\\/g;
+    bsd_glob $pattern;
+}
+
+sub clear { remove_tree(TEMP_SUBDIR, { error => \my $err }) }
+
+sub temp_file { FS->rel2abs(FS->catfile(TEMP_SUBDIR, $_[0])) }
+
+sub write_temp_file {
+    my ($name, $text) = @_;
+    -d TEMP_SUBDIR or mkdir TEMP_SUBDIR;
+    my $file = temp_file($name);
+    open my $fh, '>', $file;
+    print $fh $text;
+    close $fh;
+    return $file;
+}
+
+sub which {
+    my ($detector, $file) = @_;
+    return 0 if $^O eq 'MSWin32';
+    my $output =`which $file`;
+    for my $line (split /\n/, $output) {
+        $detector->validate_and_add($line);
+    }
+}
+
+sub env_path {
+    my ($detector, $file) = @_;
+    for my $dir (FS->path) {
+        folder($detector, $dir, $file);
+    }
+}
+
+sub extension {
+    my ($detector, $path) = @_;
+    my @exts = ('', '.exe', '.bat', '.com');
+    for my $e (@exts) {
+        $detector->validate_and_add($path . $e);
+    }
+}
+
+sub folder {
+    my ($detector, $folder, $file) = @_;
+    debug_log("folder: $folder / $file");
+    for (globq $folder) {
+        extension($detector, FS->catfile($_, $file));
+    }
+}
+
+sub normalize_path { FS->case_tolerant ? uc $_[0] : $_[0] }
+
+sub version_cmp {
+    my ($a, $b) = @_;
+    my @A = ($a =~ /([-.]|\d+|[^-.\d]+)/g);
+    my @B = ($b =~ /([-.]|\d+|[^-.\d]+)/g);
+
+    my ($A, $B);
+    while (@A and @B) {
+        $A = shift @A;
+        $B = shift @B;
+        if ($A eq '.' and $B eq '.') {
+            next;
+        } elsif ( $A eq '.' ) {
+            return -1;
+        } elsif ( $B eq '.' ) {
+            return 1;
+        } elsif ($A =~ /^\d+$/ and $B =~ /^\d+$/) {
+            if ($A =~ /^0/ || $B =~ /^0/) {
+                return $A cmp $B if $A cmp $B;
+            } else {
+                return $A <=> $B if $A <=> $B;
+            }
+        } else {
+            $A = uc $A;
+            $B = uc $B;
+            return $A cmp $B if $A cmp $B;
+        }
+    }
+    @A <=> @B;
+}
+
+1;

lib/CATS/DevEnv/Detector/Utils/Windows.pm
@@ -1,0 +1,184 @@
+package CATS::DevEnv::Detector::Utils::Windows;
+
+use strict;
+use warnings;
+
+BEGIN { $^O eq 'MSWin32' or die 'Windows only'; }
+
+use constant FS => 'File::Spec';
+
+use Win32::TieRegistry;
+use Win32::API;
+use Win32API::File qw(getLogicalDrives SetErrorMode);
+
+use CATS::DevEnv::Detector::Utils::Common;
+
+use parent qw(Exporter);
+our @EXPORT = qw(
+    registry registry_assoc registry_glob program_files drives lang_dirs
+    disable_error_dialogs disable_windows_error_reporting_ui
+);
+
+use constant REGISTRY_PREFIX => qw(
+    HKEY_LOCAL_MACHINE/SOFTWARE/
+    HKEY_LOCAL_MACHINE/SOFTWARE/Wow6432Node/
+);
+
+sub registry {
+    my ($detector, $reg, $key, $local_path, $file) = @_;
+    $local_path ||= '';
+    for my $reg_prefix (REGISTRY_PREFIX) {
+        my $registry = get_registry_obj("$reg_prefix$reg") or next;
+        my $folder = $registry->GetValue($key) or next;
+        folder($detector, FS->catdir($folder, $local_path), $file);
+    }
+}
+
+use constant REG_READONLY => {
+    Delimiter => '/',
+    Access =>
+        Win32::TieRegistry::KEY_READ
+};
+
+use constant REG_READ_WRITE => {
+    Delimiter => '/',
+    Access =>
+        Win32::TieRegistry::KEY_READ |
+        Win32::TieRegistry::KEY_WRITE
+};
+
+sub get_registry_obj {
+    my ($reg) = @_;
+    debug_log("get_registry_obj: $reg");
+    Win32::TieRegistry->new($reg, REG_READONLY);
+}
+
+sub registry_assoc {
+    my ($detector, %p) = @_;
+    my $assoc = $p{assoc} or die;
+    my $local_path = $p{local_path} // '';
+    my $file = $p{file} or die;
+    my $command = $p{command} // 'Open';
+    my $cmd_key = get_registry_obj("HKEY_CLASSES_ROOT/$assoc/Shell/$command/Command") or return;
+    my $cmd_line = $cmd_key->GetValue('') or return;
+    my ($folder) = ($cmd_line =~ /^\"([^"]+)\\[^\\]+\"/) or return;
+    folder($detector, FS->catdir($folder, $local_path), $file);
+}
+
+sub SubKeyNames_fixup {
+    my ($reg) = @_;
+    $reg = tied(%$reg) if tied(%$reg);
+    # Workaround for https://rt.cpan.org/Public/Bug/Display.html?id=97127, fixed in Win32::TieRegistry 0.27
+    use version ();
+    return $reg->SubKeyNames if version->parse($Win32::TieRegistry::VERSION) >= version->parse('0.27');
+    my @subkeys;
+    my ($nameSize, $classSize) = $reg->Information(qw(MaxSubKeyLen MaxSubClassLen));
+    while ($reg->RegEnumKeyEx(
+        scalar @subkeys, my $subkey, (my $ns = $nameSize + 1), [], my $class, (my $cl = $classSize + 1), my $time)
+    ) {
+        push @subkeys, $subkey;
+    }
+    @subkeys;
+}
+
+sub _registry_rec {
+    my ($detector, $parent, $local_path, $file, $key, @rest) = @_;
+    if (!@rest) {
+        my $folder = $parent->GetValue($key) or return;
+        folder($detector, FS->catdir($folder, $local_path), $file);
+        return;
+    }
+    my @names = $key eq '*' ? SubKeyNames_fixup($parent) : $key;
+    for my $subkey (@names) {
+        my $subreg = $parent->Open($subkey, REG_READONLY) or next;
+        debug_log('_registry_rec: ', $parent->Path(), $subkey);
+        _registry_rec($detector, $subreg, $local_path, $file, @rest)
+    }
+}
+
+sub registry_glob {
+    my ($detector, $reg_path, $local_path, $file) = @_;
+    $local_path ||= '';
+    my @r = split '/', $reg_path, -1;
+    for (REGISTRY_PREFIX) {
+        my $reg = get_registry_obj($_) or return;
+        _registry_rec($detector, $reg, $local_path, $file, @r);
+    }
+}
+
+sub program_files {
+    my ($detector, $local_path, $file) = @_;
+    my @keys = (
+        'ProgramFilesDir',
+        'ProgramFilesDir (x86)'
+    );
+    foreach my $key (@keys) {
+        registry($detector, 'Microsoft/Windows/CurrentVersion', $key, $local_path, $file);
+    }
+}
+
+sub drives {
+    my ($detector, $folder, $file) = @_;
+    $folder or die;
+    my @drives = getLogicalDrives();
+    foreach my $drive (@drives) {
+        folder($detector, FS->catdir($drive, $folder), $file);
+    }
+}
+
+sub lang_dirs {
+    my ($detector, $folder, $subfolder, $file) = @_;
+    drives($detector, FS->catfile(@$_), $file) for
+        [ 'lang', $folder, $subfolder ],
+        [ 'langs', $folder, $subfolder ],
+        [ 'lang', $folder, '*', $subfolder ],
+        [ 'langs', $folder, '*', $subfolder ];
+}
+
+sub disable_error_dialogs {
+    # https://msdn.microsoft.com/en-us/library/windows/desktop/ms680621%28v=vs.85%29.aspx
+    # SEM_FAILCRITICALERRORS
+    SetErrorMode(1 | SetErrorMode(0));
+}
+
+use constant POLICY_KEYS => qw(
+    HKEY_LOCAL_MACHINE/Software/Policies
+    HKEY_LOCAL_MACHINE/Software/Microsoft/Windows/CurrentVersion/Policies
+    HKEY_CURRENT_USER/Software/Policies
+    HKEY_CURRENT_USER/Software/Microsoft/Windows/CurrentVersion/Policies
+);
+
+sub windows_encode_console_message {
+    my ($msg) = @_;
+    my $acp = Win32::API::More->new('kernel32', 'int GetACP()')->Call;
+    my $cocp = Win32::API::More->new('kernel32', 'int GetConsoleOutputCP()')->Call;
+    Encode::from_to($msg, "CP$acp", "CP$cocp") if $acp != $cocp;
+    $msg;
+}
+
+sub disable_windows_error_reporting_ui {
+    my $wre = 'Microsoft/Windows/Windows Error Reporting';
+    for my $key (POLICY_KEYS) {
+        my $obj = get_registry_obj("$key/$wre") or next;
+        my $old_value = $obj->GetValue('DontShowUI') // next;
+        if (hex($old_value) > 0) {
+            print ' (already disabled) ';
+        }
+        else {
+            print ' (was enabled) ';
+            $obj = Win32::TieRegistry->new("$key/$wre", REG_READ_WRITE) or die $^E;
+            $obj->SetValue('DontShowUI', 1);
+        }
+        return;
+    }
+    print ' (was undefined) ';
+    for my $key (POLICY_KEYS) {
+        my $parent = Win32::TieRegistry->new($key, REG_READ_WRITE) or next;
+        my $obj = $parent->CreateKey($wre, REG_READ_WRITE) or next;
+        $obj->SetValue('DontShowUI', 1) or next;
+        return;
+    }
+    print 'failed to set: ', windows_encode_console_message($^E);
+}
+
+1;

lib/CATS/DevEnv/Detector/Utils/WindowsStub.pm
@@ -1,0 +1,21 @@
+package CATS::DevEnv::Detector::Utils::WindowsStub;
+
+use strict;
+use warnings;
+
+use parent qw(Exporter);
+our @EXPORT = qw(
+    registry registry_assoc registry_glob program_files drives lang_dirs
+    disable_error_dialogs disable_windows_error_reporting_ui
+);
+
+sub registry {}
+sub registry_assoc {}
+sub registry_glob {}
+sub program_files {}
+sub drives {}
+sub lang_dirs {}
+sub disable_windows_error_reporting_ui {}
+sub disable_error_dialogs {}
+
+1;

commit 795e0820d3c566c7553dfde1cee8c79181deeb43

install: Fix verbose mode
Broken by 041a0744f29b2270c1aa342a072d65e9c7ae6289

install.pl
@@ -33,12 +33,9 @@ GetOptions(
 ) or usage;
 usage if defined $opts{help};
 
-print "Installing cats-judge\n";
+CATS::DevEnv::Detector::Utils::set_debug(1, *STDERR) if $opts{verbose};
 
-if ($opts{verbose}) {
-    $CATS::DevEnv::Detector::Utils::log = *STDERR;
-    $CATS::DevEnv::Detector::Utils::debug = 1;
-}
+printf "Installing cats-judge%s\n", ($opts{verbose} ? ' verbosely' : '');
 
 my %filter_steps;
 if ($opts{step}) {

lib/CATS/DevEnv/Detector/Utils.pm
@@ -11,7 +11,14 @@ use parent qw(Exporter);
 our @EXPORT = qw(
     TEMP_SUBDIR temp_file write_temp_file version_cmp clear normalize_path globq
     which env_path folder debug_log
+    set_debug
     registry registry_assoc registry_glob program_files drives lang_dirs
 );
+
+sub set_debug {
+	my ($debug, $stderr) = @_;
+	$CATS::DevEnv::Detector::Utils::Common::debug = $debug;
+	$CATS::DevEnv::Detector::Utils::Common::log = $stderr;
+}
 
 1;