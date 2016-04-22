klenin/cats-judge

commit c03823041585bf30bc8e2179837c51fbaff2168b

Make solution as optional argument

judge.pl
@@ -949,7 +949,8 @@ sub process_request
     }
     $judge->save_log_dump($r, $log->{dump});
     $judge->set_request_state($r, $state, %$r);
-    return if $state == $cats::st_unhandled_error;
+    log_msg("error: missing solution\n") if ($judge->{de} || $judge->{testset}) && !$judge->{solution};
+    return if !$judge->{solution} || $state == $cats::st_unhandled_error;
 
     log_msg("test log:\n");
     if ($r->{fname} =~ /[^_a-zA-Z0-9\.\\\:\$]/) {
@@ -1001,7 +1002,7 @@ sub usage
     print <<"USAGE";
 Usage:
     $cmd
-    $cmd --problem <zip_or_directory> --solution <file> [--de <de_code>] [--testset <testset>] [--db]
+    $cmd --problem <zip_or_directory> [--solution <file> [--de <de_code>] [--testset <testset>]] [--db]
     $cmd --print-config <regexp>
     $cmd --set-config <name>=<value> ...
     $cmd --help|-?
@@ -1043,7 +1044,7 @@ ensure_dir('rundir', $cfg->rundir);
 
 $log->init($cfg->logdir);
 
-my $local = defined $opts{solution} && defined $opts{problem};
+my $local = defined $opts{problem};
 
 CATS::DB::sql_connect({
     ib_timestampformat => $CATS::Judge::Base::timestamp_format,

lib/CATS/Judge/Local.pm
@@ -56,7 +56,7 @@ sub select_request {
     eval { $self->{parser}->parse; };
     die "Problem parsing failed: $@" if $@;
 
-    open FILE, $self->{solution} or die "Couldn't open file: $!";
+    !$self->{solution} or open FILE, $self->{solution} or die "Couldn't open file: $!";
     {
         id => 0,
         problem_id => $self->get_problem_id,
@@ -66,7 +66,7 @@ sub select_request {
         run_all_tests => 1,
         status => $cats::problem_st_ready,
         fname => $self->{solution},
-        src => (join '', <FILE>),
+        src => $self->{solution} ? (join '', <FILE>) : '',
         de_id => $self->{de},
     };
 }
@@ -86,7 +86,7 @@ sub set_DEs {
 sub set_def_DEs {
     my ($self, $cfg_def_DEs) = @_;
     $self->{def_DEs} = $cfg_def_DEs;
-    $self->{de} = $self->auto_detect_de($self->{solution}) if !$self->{de};
+    $self->{de} = $self->auto_detect_de($self->{solution}) if !$self->{de} && $self->{solution};
 }
 
 sub pack_problem_source

commit a69f1a5ce597543aac6805ebb23262002320b8e8

Fix server mode
Broken by c03823041585bf30bc8e2179837c51fbaff2168b

judge.pl
@@ -307,7 +307,7 @@ sub generate_test
     {
         $test->{gen_group} and return undef;
         $out = 'stdout1.txt';
-        $redir = " -so:$out -ho:1";
+        $redir = " --out=nul --out=$out";
     }
     my $sp_report = $spawner->execute(
         $generate_cmd, {
@@ -949,8 +949,7 @@ sub process_request
     }
     $judge->save_log_dump($r, $log->{dump});
     $judge->set_request_state($r, $state, %$r);
-    log_msg("error: missing solution\n") if ($judge->{de} || $judge->{testset}) && !$judge->{solution};
-    return if !$judge->{solution} || $state == $cats::st_unhandled_error;
+    return if !$r->{src} || $state == $cats::st_unhandled_error;
 
     log_msg("test log:\n");
     if ($r->{fname} =~ /[^_a-zA-Z0-9\.\\\:\$]/) {
@@ -1029,7 +1028,6 @@ usage if defined $opts{help};
     $cfg->read_file($cfg_file, $opts{'set-config'});
 }
 
-
 if (defined(my $regexp = $opts{'print-config'})) {
     $cfg->print_params($regexp);
     exit;
@@ -1045,6 +1043,10 @@ ensure_dir('rundir', $cfg->rundir);
 $log->init($cfg->logdir);
 
 my $local = defined $opts{problem};
+
+if ($local && !$opts{solution}) {
+    $opts{$_} && print "error: --$_ without --solution\n" for qw(de testset);
+}
 
 CATS::DB::sql_connect({
     ib_timestampformat => $CATS::Judge::Base::timestamp_format,