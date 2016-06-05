c08515a7ef7f1c45d9f7ae37c3279bbddcee1eac
Alexander S. Klenin <klenin@gmail.com>
2016-06-06 07:10:27 +1000

    Spawner::Platform: Fix uname usage

--- lib/CATS/Spawner/Platform.pm
+++ lib/CATS/Spawner/Platform.pm
@@ -9,7 +9,7 @@ use POSIX qw();
 sub get {
     $^O eq 'MSWin32' ? 'win32' :
     $^O eq 'darwin' ? 'darwin' :
-    $^O eq 'linux' ? (POSIX::uname[4] eq 'x86_64' ? 'linux-amd64' : 'linux-i386') :
+    $^O eq 'linux' ? ((POSIX::uname())[4] eq 'x86_64' ? 'linux-amd64' : 'linux-i386') :
     undef
 }
 
