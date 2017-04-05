a56d586aa56e57a9faf1ae9f79d4e1ef565d72fd
Lukaviy <jiukaviy@gmail.com>
2017-04-05 18:18:08 +1000

    Fix stdout logging for interactive problems

--- judge.pl
+++ judge.pl
@@ -94,7 +94,7 @@ sub get_run_params {
 
     my $names = $get_names->($ps) or return;
 
-    my $global_opts = $run_info->{method} == $cats::rm_interactive ? { %$limits, idle_time_limit => 1 }  : {};
+    my $global_opts = { ($run_info->{method} == $cats::rm_interactive ? ( %$limits, idle_time_limit => 1 ) : ()), stdout => 'nul' };
     my $solution_opts = $run_info->{method} == $cats::rm_interactive ? $other_opts : { %$limits, %$other_opts };
     my @programs;
 
