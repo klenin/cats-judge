4c200171a8df91c12b83a83244a50f638d1e5cf6
TheLocky <xtpvxt@gmail.com>
2017-04-22 15:57:45 +1000

    Spawner::Dafault: Add error code for simple report

--- lib/CATS/Spawner/Default.pm
+++ lib/CATS/Spawner/Default.pm
@@ -238,6 +238,7 @@ my @legacy_required_fields = qw(
     PeakMemoryUsed
     Written
     TerminateReason
+    ExitCode
     ExitStatus
     SpawnerError
 );
@@ -273,7 +274,8 @@ sub parse_legacy_report {
     #PeakMemoryUsed:        20.140625 (Mb)
     #Written:               0.000000 (Mb)
     #TerminateReason:       TimeLimitExceeded
-    #ExitStatus:            0
+    #ExitCode               0
+    #ExitStatus:            SIGKILL
     #----------------------------------------------
     #SpawnerError:          <none>
 
@@ -320,8 +322,8 @@ sub parse_legacy_report {
             write => mb_to_bytes($raw_report->{WriteLimit}),
         },
         terminate_reason => $tr,
+        exit_code => $raw_report->{ExitCode},
         exit_status => $raw_report->{ExitStatus},
-        exit_code => $raw_report->{ExitStatus},
         consumed => {
             user_time => $raw_report->{UserTime},
             memory => mb_to_bytes($raw_report->{PeakMemoryUsed}),
