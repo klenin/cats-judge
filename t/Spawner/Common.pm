4c05584195039478656e61ea3a5b9645438a02ff
TheLocky <xtpvxt@gmail.com>
2017-05-20 12:00:40 +1000

    multiple.t: Extract base functions and variables

--- t/Spawner/Common.pm
+++ t/Spawner/Common.pm
@@ -3,10 +3,12 @@ package Common;
 use strict;
 use warnings;
 
+use Exporter qw(import);
 use File::Spec;
-use constant FS => 'File::Spec';
-
 use FindBin qw($Bin);
+use Test::More;
+use constant FS => 'File::Spec';
+use constant MB => 1024 * 1024;
 
 my $root;
 BEGIN { $root = FS->catdir($Bin, '..', '..'); };
@@ -14,16 +16,39 @@ BEGIN { $root = FS->catdir($Bin, '..', '..'); };
 use lib FS->catdir($root, 'lib');
 use lib FS->catdir($root, 'lib', 'cats-problem');
 
+use CATS::FileUtil;
+use CATS::Judge::Config;
+use CATS::Loggers;
 use CATS::Spawner::Builtin;
+use CATS::Spawner::Const ':all';
 use CATS::Spawner::Default;
 use CATS::Spawner::Platform;
 use CATS::Spawner::Program;
-use CATS::FileUtil;
-use CATS::Loggers;
-use CATS::Judge::Config;
 
-use Exporter qw(import);
-our @EXPORT = qw($tmpdir $fu $perl $sp $cfg);
+our @EXPORT = qw(
+    $cfg
+    $compile_plan
+    $exe
+    $fu
+    $gcc
+    $is_win
+    $perl
+    $sp
+    $spr
+    $tmpdir
+    @gcc_opts
+    MB
+    clear_tmpdir
+    compile
+    items_ok
+    items_ok_plan
+    make_test_file
+    program
+    run_sp
+    run_sp_multiple
+    run_subtest
+    tmp_name
+);
 
 our $tmpdir;
 BEGIN {
@@ -31,6 +56,9 @@ BEGIN {
      -d $tmpdir or mkdir $tmpdir or die 'Unable to create temporary directory';
 }
 END { -d $tmpdir and rmdir $tmpdir }
+
+our $is_win = ($^O eq 'MSWin32');
+our $exe = $is_win ? '.exe' : '';
 
 our $cfg = CATS::Judge::Config->new;
 our $fu = CATS::FileUtil->new({ logger => CATS::Logger::Die->new });
@@ -40,5 +68,99 @@ our $sp = FS->catdir($root, CATS::Spawner::Platform::get_path);
 my $judge_cfg = FS->catdir($root, 'config.xml');
 open my $cfg_file, '<', $judge_cfg or die "Couldn't open $judge_cfg";
 $cfg->read_file($cfg_file, {});
+
+our ($gcc, @gcc_opts) = split ' ', $cfg->defines->{'#gnu_cpp'};
+
+our $builtin_runner = CATS::Spawner::Builtin->new({
+    logger => CATS::Logger::Die->new,
+    run_temp_dir => $tmpdir,
+    run_method => 'system'
+});
+
+our $spr = CATS::Spawner::Default->new({
+    logger => CATS::Logger::Count->new,
+    path => $sp,
+    save_stdout => [ $tmpdir, 'stdout.txt' ],
+    save_stderr => [ $tmpdir, 'stderr.txt' ],
+    save_report => [ $tmpdir, 'report.txt' ],
+    #debug => 1,
+    json => 1,
+});
+
+sub clear_tmpdir {
+    push @_, '*' if @_ < 1;
+    $fu->remove([ $tmpdir, $_ ]) for (@_);
+}
+
+sub items_ok_plan { 1 + $_[0] * 3 }
+
+sub items_ok {
+    my ($r, $apps, $msg) = @_;
+    is scalar @{$r->items}, scalar @$apps, 'report count == programs count';
+    for my $i (0 .. scalar @{$r->items} - 1) {
+        my $ri = $r->items->[$i];
+        is $r->exit_code, 0, "$msg spawner exit code";
+        is_deeply $ri->{errors}, [], "$msg no errors";
+        is $ri->{terminate_reason}, $apps->[$i]->{tr} // $TR_OK, "$msg TR_CODE";
+    }
+    $r->items;
+}
+
+sub program { CATS::Spawner::Program->new(@_) }
+
+our $compile_plan = items_ok_plan(1) + 2;
+
+sub compile {
+    my ($src, $out, $skip, $flags) = @_;
+    my $fullsrc = FS->catdir($Bin, 'cpp', $src);
+    $out = FS->catdir($tmpdir, $out);
+    my $app = program($gcc, [ @{$flags // []}, @gcc_opts, '-O0', '-o', $out, $fullsrc ]);
+    my $r = $builtin_runner->run(undef, $app);
+    items_ok($r, [ $app ], "$src compile");
+    my $compile_success = 1;
+    is $r->items->[0]->{exit_status}, 0, 'compile exit code' or $compile_success = 0;
+    ok -x $out, 'compile success' or $compile_success = 0;
+    ($skip and skip "$src compilation failed", $skip - $compile_plan) unless $compile_success;
+    $out;
+}
+
+sub run_sp {
+    my ($globals, $application, $args, $opts, $tr) = @_;
+    my $app = program($application, $args, $opts)->set_expected_tr($tr // $TR_OK);
+    my $r = $spr->run($globals, $app);
+    items_ok($r, [ $app ], (FS->splitpath($application))[2]);
+}
+
+sub run_sp_multiple {
+    my ($globals, $apps, $msg) = @_;
+    my $r = $spr->run($globals, @$apps);
+    items_ok($r, $apps, join ', ', map { (FS->splitpath($_->application))[2] } @$apps );
+}
+
+sub tmp_name {
+    my @chars = ('A'..'Z', 'a'..'z', '0'..'9');
+    FS->catdir($tmpdir, join '', (map @chars[rand @chars], 1..10), '.tmp');
+}
+
+sub make_test_file {
+    my ($line, $count, $endl) = @_;
+    $endl //= "\n";
+
+    my $filename = tmp_name;
+    open my $fh, '>', $filename;
+    print $fh "$line$endl" for 1..$count - 1;
+    print $fh $line;
+    $filename;
+}
+
+sub run_subtest {
+    my ($name, $plan, $sub) = @_;
+    subtest $name => sub {
+        SKIP: {
+            plan tests => $plan;
+            $sub->($plan);
+        }
+    };
+}
 
 1;
--- t/Spawner/multiple.t
+++ t/Spawner/multiple.t
@@ -11,106 +11,9 @@ use FindBin qw($Bin);
 BEGIN { require File::Spec->catdir($Bin, 'Common.pm'); Common->import; }
 
 use CATS::Spawner::Const ':all';
-use constant MB => 1024 * 1024;
 
-my $is_win = ($^O eq 'MSWin32');
-my $exe = $is_win ? '.exe' : '';
-
-my ($gcc, @gcc_opts) = split ' ', $cfg->defines->{'#gnu_cpp'};
 ok -x $sp, 'sp exists' or exit;
 ok -x $gcc, 'gcc exists' or exit;
-
-my $builtin_runner = CATS::Spawner::Builtin->new({
-    logger => CATS::Logger::Die->new,
-    run_temp_dir => $tmpdir,
-    run_method => 'system'
-});
-
-my $spr = CATS::Spawner::Default->new({
-    logger => CATS::Logger::Count->new,
-    path => $sp,
-    save_stdout => [ $tmpdir, 'stdout.txt' ],
-    save_stderr => [ $tmpdir, 'stderr.txt' ],
-    save_report => [ $tmpdir, 'report.txt' ],
-    #debug => 1,
-    json => 1,
-});
-
-sub clear_tmpdir {
-    push @_, '*' if @_ < 1;
-    $fu->remove([ $tmpdir, $_ ]) for (@_);
-}
-
-sub items_ok_plan { 1 + $_[0] * 3 }
-
-sub items_ok {
-    my ($r, $apps, $msg) = @_;
-    is scalar @{$r->items}, scalar @$apps, 'report count == programs count';
-    for my $i (0 .. scalar @{$r->items} - 1) {
-        my $ri = $r->items->[$i];
-        is $r->exit_code, 0, "$msg spawner exit code";
-        is_deeply $ri->{errors}, [], "$msg no errors";
-        is $ri->{terminate_reason}, $apps->[$i]->{tr} // $TR_OK, "$msg TR_CODE";
-    }
-    $r->items;
-}
-
-sub program { CATS::Spawner::Program->new(@_) }
-
-my $compile_plan = items_ok_plan(1) + 2;
-
-sub compile {
-    my ($src, $out, $skip, $flags) = @_;
-    my $fullsrc = FS->catdir($Bin, 'cpp', $src);
-    $out = FS->catdir($tmpdir, $out);
-    my $app = program($gcc, [ @{$flags // []}, @gcc_opts, '-O0', '-o', $out, $fullsrc ]);
-    my $r = $builtin_runner->run(undef, $app);
-    items_ok($r, [ $app ], "$src compile");
-    my $compile_success = 1;
-    is $r->items->[0]->{exit_status}, 0, 'compile exit code' or $compile_success = 0;
-    ok -x $out, 'compile success' or $compile_success = 0;
-    ($skip and skip "$src compilation failed", $skip - $compile_plan) unless $compile_success;
-    $out;
-}
-
-sub run_sp {
-    my ($globals, $application, $args, $opts, $tr) = @_;
-    my $app = program($application, $args, $opts)->set_expected_tr($tr // $TR_OK);
-    my $r = $spr->run($globals, $app);
-    items_ok($r, [ $app ], (FS->splitpath($application))[2]);
-}
-
-sub run_sp_multiple {
-    my ($globals, $apps, $msg) = @_;
-    my $r = $spr->run($globals, @$apps);
-    items_ok($r, $apps, join ', ', map { (FS->splitpath($_->application))[2] } @$apps );
-}
-
-sub tmp_name {
-    my @chars = ('A'..'Z', 'a'..'z', '0'..'9');
-    FS->catdir($tmpdir, join '', (map @chars[rand @chars], 1..10), '.tmp');
-}
-
-sub make_test_file {
-    my ($line, $count, $endl) = @_;
-    $endl //= "\n";
-
-    my $filename = tmp_name;
-    open my $fh, '>', $filename;
-    print $fh "$line$endl" for 1..$count - 1;
-    print $fh $line;
-    $filename;
-}
-
-sub run_subtest {
-    my ($name, $plan, $sub) = @_;
-    subtest $name => sub {
-        SKIP: {
-            plan tests => $plan;
-            $sub->($plan);
-        }
-    };
-}
 
 run_subtest 'HelloWorld', $compile_plan + items_ok_plan(1) + 1, sub {
     my $hw = compile('helloworld.cpp', 'helloworld' . $exe, $_[0]);
@@ -477,7 +380,7 @@ SKIP: {
     my $test_src = FS->catdir($Bin, 'cpp', 'helloworld.cpp');
     my $test_out = FS->catdir($tmpdir, "mingw_m32_test$exe");
     my $gcc_prog = program($gcc, [ '-m32', @gcc_opts, '-O0', '-o', $test_out, $test_src ]);
-    my $gcc_test = $builtin_runner->run(undef, $gcc_prog);
+    my $gcc_test = $Common::builtin_runner->run(undef, $gcc_prog);
     skip('bad -m32 option support', 3) if $gcc_test->exit_code != 0;
 
     run_subtest 'Win32 compliant stack segment', $compile_plan + 2, sub {
