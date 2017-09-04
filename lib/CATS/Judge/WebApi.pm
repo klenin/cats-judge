74f180820d968cc99fa331032f7113ab184a6388
Alexander S. Klenin <klenin@gmail.com>
2017-09-05 03:15:02 +1000

    WebApi: Normalize request parameters
    Do not allow undefs and blessed objects.

--- lib/CATS/Judge/WebApi.pm
+++ lib/CATS/Judge/WebApi.pm
@@ -160,7 +160,7 @@ sub set_request_state {
         state => $state,
         problem_id => $p{problem_id},
         contest_id => $p{contest_id},
-        failed_test => $p{failed_test},
+        failed_test => $p{failed_test} // '',
         sid => $self->{sid},
     ]);
 
@@ -174,7 +174,7 @@ sub select_request {
         f => 'api_judge_select_request',
         sid => $self->{sid},
         de_version => $self->{dev_env}->version,
-        CATS::JudgeDB::get_de_bitfields_hash(@{$self->{de_bitmap}}),
+        map "$_", CATS::JudgeDB::get_de_bitfields_hash(@{$self->{de_bitmap}}),
     ]);
 
     if ($response->{error}) {
