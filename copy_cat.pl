448e888ae1bd6e7b225d8dd256a6f5a3ee4ffc8e
Alexander Klenin <klenin@gmail.com>
2020-02-29 20:42:53 +1000

    copy_cat: Add -l option to copy a single line

--- copy_cat.pl
+++ copy_cat.pl
@@ -20,12 +20,13 @@ sub usage {
 Copies input to either output or STDOUT.
 
 Usage:
-    $FindBin::Script -i <input> -o <output> -m <mode>
+    $FindBin::Script -i <input> -o <output> -m <mode> [-l <line>]
 
 Options:
     #-i#    Input file
     #-o#    Output file
-    #-m#    *STDOUT, *NONE or output file name
+    #-m#    *STDOUT, *NONE or FILE
+    #-l#    Copy only line with given number
 USAGE
     ;
     $text =~ s/#(\S+)#/CATS::ConsoleColor::colored($1, 'bold white')/eg;
@@ -37,12 +38,27 @@ GetOptions(
     'i=s' => \(my $input),
     'o=s' => \(my $output),
     'm=s' => \(my $mode = ''),
+    'l=i' => \(my $line_number = ''),
 );
 
 $input && ($output || $mode) or usage;
 
-if ($mode =~ /^\*STDOUT|\*NONE$/) {
-    open my $fin, '<', $input;
+my $is_stdout = $mode =~ /^\*STDOUT|\*NONE$/;
+if ($line_number) {
+    my $fout;
+    if ($is_stdout) {
+        $fout = *STDOUT;
+    }
+    else {
+        open $fout, '>', $output or die $!;
+    }
+    open my $fin, '<', $input or die $!;
+    for (my $line = 1; <$fin>; ++$line) {
+        print $fout $_ if $line == $line_number;
+    }
+}
+elsif ($is_stdout) {
+    open my $fin, '<', $input or die $!;
     print while <$fin>;
 }
 else {
